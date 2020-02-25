85b417f197faa5b499826c4ea2d57c33081ec9db
Smith <dusankovacevic47@gmail.com>
2020-02-25 17:10:46 +0100

    Walking Skeleton 2 semantic UI added, need fixnig

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,40 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios'
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
 
 class App extends Component {
+  state = {
+    values: []
+  };
 
-    state = {
-        values: []
-    }
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/values").then((response) => {
+      this.setState({
+        values: response.data
+      });
+    });
+  }
 
-    componentDidMount(){
-        axios.get('http://localhost:5000/api/values')
-            .then((response) => {
-                this.setState({
-                    values : response.data
-                })
-            }) 
-    }
-
-    render(){
-
-        return (
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <ul>
-                  {this.state.values.map((value: any) => (
-                    <li key={value.id}>{value.name}</li>
-                  ))}
-              </ul>
-            </header>
-          </div>
-        );
-    }
-  
+  render() {
+    return (
+      <div>
+        <Header as="h2">
+          <Icon as="users" />><Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
+      </div>
+    );
+  }
 }
 
 export default App;
